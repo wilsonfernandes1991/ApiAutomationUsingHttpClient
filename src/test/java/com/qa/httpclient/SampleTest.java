9fb8f0e7fcf05b746c8a3077cab7a93613843fa7
Wilson <fernandes.wilson57@gmail.com>
2018-03-10 17:17:00 +0530

    Basic Setup commit

---
+++ src/test/java/com/qa/httpclient/SampleTest.java
@@ -0,0 +1,12 @@
+package com.qa.httpclient;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Wilson");
+
+	}
+
+}
